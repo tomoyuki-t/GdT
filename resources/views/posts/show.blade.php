5934a0b4548306fc79001b6d3acb003e48b48b0e
tomoyuki-t <tomoyuki_takemoto@eparkgourmet.jp>
2021-12-08 17:18:34 +0900

    display user_name on posts.show

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -3,11 +3,12 @@
 
 @section('content')
 <div class='show-container'>
-    <h1>{{ $post->title }}</h1>
-    <h2>
+    <h1>{{ $post->user->name }}</h1>
+    <h2>{{ $post->title }}</h2>
+    <h3>
         {{ $post->area->prefecture}}<br>
         {{ $post->body }}
-    </h2>
+    </h3>
 </div>
     
 @endsection
